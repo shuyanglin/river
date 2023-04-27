b2bdea096f20831c0475c79fb74468a894e9297e
shuyanglin <shuyanglin.tw@gmail.com>
2023-04-27 09:07:25 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("key: " +process.env.REACT_APP_OPENAPI_KEY);
+  const API_KEY = process.env.REACT_APP_OPENAPI_KEY;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {API_KEY}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
